0bb766560381943c93230064130b3ac23e49bb43
Federico Guigou <fefogs@gmail.com>
2025-06-11 12:01:21 -0300

    se agrega js para allocate.blade

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/maquinas.js',
                 'resources/js/obras.js',
+                'resources/js/allocate.js',
             ],
             refresh: true,
         }),
